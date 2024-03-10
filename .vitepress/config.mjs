ad1edaec921995092b88c559d79eafda62dac0ab
stormblinger-ai <Dayang747@163.com>
2024-03-10 15:11:13 +0800

    ds

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -71,9 +71,27 @@ export default defineConfig({
           text: 'Python函数与面向对象',
           collapsed: false,
           items: [
-            { text: 'Index', link: '/guide/' },
-            { text: 'One', link: '/guide/one' },
-            { text: 'Two', link: '/guide/two' }
+            { text: 'Python 函数', link: '/Python/15' },
+            { text: 'Python 中的 Lambda 函数和匿名函数', link: '/Python/16' },
+            { text: 'Python 中的变量作用域', link: '/Python/17' },
+            { text: 'Python 中的__main__和__name__', link: '/Python/18' },
+            { text: 'Python 生成器函数', link: '/Python/19' },
+            { text: 'Python 列表推导', link: '/Python/20' },
+            { text: 'Python 中的递归', link: '/Python/21' },
+            { text: 'Python 错误类型', link: '/Python/22' },
+            { text: 'Python 中的异常处理', link: '/Python/23' },
+            { text: 'Python assert语句', link: '/Python/24' },
+            { text: 'Python 类', link: '/Python/25' },
+            { text: 'Python 中的继承', link: '/Python/26' },
+            { text: 'Python 公共、受保护、私有成员', link: '/Python/27' },
+            { text: 'Python 中的装饰器', link: '/Python/28' },
+            { text: 'Python 属性装饰器——@property', link: '/Python/29' },
+            { text: 'Python 类方法装饰器@classmethod', link: '/Python/30' },
+            { text: 'Python 中使用@staticmethod装饰器定义静态方法', link: '/Python/31' },
+            { text: 'Python 魔术方法', link: '/Python/32' },
+
+
+
           ]
         },
         {
